1846fad44e2309e64846b6075967a08f878a7fda
Rachael Ayon <rachaele23@gmail.com>
2020-07-12 21:58:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,11 @@ function addElementToEndOfArray() {
   var chocolateBars.push("Milky Way")
   console.log("chocolateBars");
 }
+function destructivelyAddElementToBeginningOfArray() {
+  var chocolateBars.push("Milky Way")
+  console.log("chocolateBars");
+}
+function accessElementInArray() {
+  var bio = "The best candy";
+  console.log(chocolateBars[4] + bio)
+}
